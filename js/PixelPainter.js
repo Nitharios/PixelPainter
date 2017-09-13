af1b0b811c2ee06c3071d45f49f81a21874b7dd5
Brendan Hazama <brendanhazama@Brendans-MacBook-Pro.local>
2017-09-12 19:17:55 -1000

    new

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -10,11 +10,11 @@ var buttonDiv = document.createElement('div');
 var fillButton = document.createElement('button');
 var eraseButton = document.createElement('button');
 var clearButton = document.createElement('button');
+var saveButton = document.createElement("button");
+var loadButton = document.createElement("button");
+var currentColor = 'rgb(255, 255, 255)';
 var mouseClick = false;
-var currentColor;
 var colorArray = [];
-
-
 
 var colorArr = ['262c04', 'ffc0cb', 'f2df4f', 'eeeeee', 'c6e2ff',
                 '4169e1', '3b411d', 'f10714', '0d8163', '255083',
@@ -32,7 +32,6 @@ painterBody.appendChild(painterCanvas);
 painterPalette.className = 'painterPalette';
 painterBody.appendChild(painterPalette);
 
-
 // Create grid canvas
 function canvasGrid(height, width) {
   for(var i = 0; i < height; i++) {
@@ -42,6 +41,7 @@ function canvasGrid(height, width) {
     for(var j = 0; j < width; j++) {
       var canvasCell = document.createElement("div");
       canvasCell.className = "canvasCell";
+      canvasCell.style.backgroundColor = 'rgb(255, 255, 255)';
       canvasCell.addEventListener('click', function() {
         if (mouseClick === false) {
           event.target.style.backgroundColor = currentColor;
@@ -91,111 +91,63 @@ buttonDiv.className = 'buttonDiv';
 painterPalette.appendChild(buttonDiv);
 
 //BUTTONS
-// fillButton.className = 'buttons';
-// fillButton.id = 'fillButton';
-// fillButton.innerHTML = 'Fill';
-
-// var canvasCellArr = document.getElementsByClassName('canvasCell');
-
-// fillButton.addEventListener('click', function() {
-//   for (var i = 0; i < canvasCellArr.length; i++) {
-//     if (canvasCellArr[i].style.backgroundColor === 'ffffff') {
-//       console.log(currentColor);
-//       canvasCellArr[i].style.backgroundColor = currentColor;
-//     };
-//   };
-//   // console.log(currentColor);
-// });
-// buttonDiv.appendChild(fillButton);
-
 eraseButton.className = 'buttons';
 eraseButton.id = 'eraseButton';
 eraseButton.innerHTML = 'Erase';
 eraseButton.addEventListener('click', function() {
-  currentColor = 'ffffff';
+  currentColor = 'rgb(255, 255, 255)';
   mouseClick = false;
 });
 buttonDiv.appendChild(eraseButton);
+
+fillButton.className = 'buttons';
+fillButton.id = 'fillButton';
+fillButton.innerHTML = 'Fill';
+
+fillButton.addEventListener('click', function() {
+  var canvasCellArr = document.getElementsByClassName('canvasCell');
+  for (var i = 0; i < canvasCellArr.length; i++) {
+    if (canvasCellArr[i].style.backgroundColor === 'rgb(255, 255, 255)') {
+      console.log(currentColor);
+      canvasCellArr[i].style.backgroundColor = currentColor;
+    };
+  };
+  // console.log(currentColor);
+});
+buttonDiv.appendChild(fillButton);
 
 clearButton.className = 'buttons';
 clearButton.id = 'clearButton';
 clearButton.innerHTML = 'Clear';
 clearButton.addEventListener('click', function() {
   for (var i = 0; i < document.getElementsByClassName('canvasCell').length; i++) {
-    document.getElementsByClassName('canvasCell')[i].style.backgroundColor = 'ffffff'
+    document.getElementsByClassName('canvasCell')[i].style.backgroundColor = 'rgb(255, 255, 255)'
   }
   resetPalette();
-  currentColor = 'ffffff';
+  currentColor = 'rgb(255, 255, 255)';
   mouseClick = false;
 });
 buttonDiv.appendChild(clearButton);
 
-
-
-function resetPalette() {
-  for (var i = 0; i < document.getElementsByClassName('paletteCell').length; i++) {
-    document.getElementsByClassName('paletteCell')[i].style.width = '40px';
-    document.getElementsByClassName('paletteCell')[i].style.height = '40px';
-    document.getElementsByClassName('paletteCell')[i].style.border = '0px';
-  }
-}
-
-//CREATING GRID
-/*function ppCanvas(pixelNumber){
-  for(var i = 0; i < pixelNumber; i++){
-    var canvasCell = document.createElement("div");
-    canvasCell.className = "canvasCell";
-    painterCanvas.appendChild(canvasCell);
-    canvasCell.addEventListener("click", function() {
-      event.target.style.backgroundColor = currentColor;
-    })
-  }
-}
-*/
-//ppCanvas(99);
-
-//CREATING COLOR PALLETE
-/* function ppColor(pixelNumber){
-  for(var i = 0; i < pixelNumber; i++){
-    var paletteCell = document.createElement("div");
-    paletteCell.className = "paletteCell";
-    painterPalette.appendChild(paletteCell);
-    paletteCell.style.backgroundColor = colorArr[i];
-    paletteCell.addEventListener("click", function() {
-      currentColor = event.target.style.backgroundColor;
-      console.log(currentColor);
-    });
-  }
-}
-
-ppColor(16);
-*/
-
-
-
-var saveButton = document.createElement("button");
 saveButton.id = "saveButton";
 saveButton.className = "buttons";
 saveButton.innerHTML = "Save";
 saveButton.addEventListener('click', savePic);
 buttonDiv.appendChild(saveButton);
 
-function savePic(){
-  var currentPic = document.getElementsByClassName("canvasCell");
-  for (var i = 0; i < currentPic.length; i++) {
-    colorArray.push(currentPic[i].style.backgroundColor);
-  }
-  console.log(colorArray);
-  mouseClick = false;
-  console.log(mouseClick);
-}
-
-var loadButton = document.createElement("button");
 loadButton.id = "loadButton";
 loadButton.className = "buttons";
 loadButton.innerHTML = "Load";
 loadButton.addEventListener("click", loadPic);
 buttonDiv.appendChild(loadButton);
+
+function savePic(){
+  var currentPic = document.getElementsByClassName("canvasCell");
+  for (var i = 0; i < currentPic.length; i++) {
+    colorArray.push(currentPic[i].style.backgroundColor);
+  }
+  mouseClick = false;
+}
 
 function loadPic(){
   var currentPic = document.getElementsByClassName("canvasCell");
@@ -204,6 +156,13 @@ function loadPic(){
   }
   colorArray = [];
   mouseClick = false;
-  console.log(mouseClick);
+}
+
+function resetPalette() {
+  for (var i = 0; i < document.getElementsByClassName('paletteCell').length; i++) {
+    document.getElementsByClassName('paletteCell')[i].style.width = '40px';
+    document.getElementsByClassName('paletteCell')[i].style.height = '40px';
+    document.getElementsByClassName('paletteCell')[i].style.border = '0px';
+  }
 }
 
