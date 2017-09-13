7043bdd169eb750696941542961ff4b49885440c
Nathan N <Nitharios@users.noreply.github.com>
2017-09-13 11:19:54 -1000

    Added comments and improved functionality

--- js/PixelPainterB.js
+++ js/PixelPainterB.js
@@ -3,14 +3,6 @@
 // erase
 // clear
 
-/* 
-Nathan's notes:
-  - Added a previousColor variable to store the previously selected color which replaces previous functionality of only filling in WHITE spaces
-  - canvasGrid has a hidden background that I was playing with (if you set backgroundColor of Clear function to null, it will reveal the picture on the page) --> document.getElementsByClassName('canvasCell')[i].style.backgroundColor = 'rgb(255, 255, 255)' <-- set this to null;
-  - If you set the backgroundColor of canvasCell (or any div) to null, it will reveal the background of that div or body
-*/
-
-
 var painterBody = document.getElementById('pixelPainter');
 var painterCanvas = document.createElement('div');
 var painterPalette = document.createElement('div');
@@ -18,13 +10,9 @@ var buttonDiv = document.createElement('div');
 var fillButton = document.createElement('button');
 var eraseButton = document.createElement('button');
 var clearButton = document.createElement('button');
-var saveButton = document.createElement("button");
-var loadButton = document.createElement("button");
-var previousColor = 'rgb(255, 255, 255)';
-var currentColor = 'rgb(255, 255, 255)';
 var mouseClick = false;
-// Used to save current picture on canvas
-var colorArray = [];
+var currentColor = 'ffffff';
+
 
 var colorArr = ['262c04', 'ffc0cb', 'f2df4f', 'eeeeee', 'c6e2ff',
                 '4169e1', '3b411d', 'f10714', '0d8163', '255083',
@@ -32,11 +20,16 @@ var colorArr = ['262c04', 'ffc0cb', 'f2df4f', 'eeeeee', 'c6e2ff',
                 '00fa9a', '00ffff', '666699','cc9900','#ffff99',
                 'ff3300', '669900', 'cc3399', 'ffcccc', '999966'];
 
+// add ids to each created element
+// painterBody.id = 'painterBody';
+// document.body.appendChild(painterBody);
+
 painterCanvas.className = 'painterCanvas';
 painterBody.appendChild(painterCanvas);
 
 painterPalette.className = 'painterPalette';
 painterBody.appendChild(painterPalette);
+
 
 // Create grid canvas
 function canvasGrid(height, width) {
@@ -47,7 +40,6 @@ function canvasGrid(height, width) {
     for(var j = 0; j < width; j++) {
       var canvasCell = document.createElement("div");
       canvasCell.className = "canvasCell";
-      canvasCell.style.backgroundColor = 'rgb(255, 255, 255)';
       canvasCell.addEventListener('click', function() {
         if (mouseClick === false) {
           event.target.style.backgroundColor = currentColor;
@@ -81,7 +73,6 @@ function paletteGrid(height, width) {
         event.target.style.width = '36px';
         event.target.style.height = '36px';
         event.target.style.border = '2px solid black';
-        previousColor = currentColor;
         currentColor = event.target.style.backgroundColor;
         mouseClick = false;
       });
@@ -91,7 +82,6 @@ function paletteGrid(height, width) {
   }
 }
 
-// Sets canvasGrid and paletteGrid sizes
 canvasGrid(20,20);
 paletteGrid(5,5);
 
@@ -99,80 +89,46 @@ buttonDiv.className = 'buttonDiv';
 painterPalette.appendChild(buttonDiv);
 
 // BUTTONS
-eraseButton.className = 'buttons';
-eraseButton.id = 'eraseButton';
-eraseButton.innerHTML = 'Erase';
-eraseButton.addEventListener('click', function() {
-  currentColor = 'rgb(255, 255, 255)';
-  mouseClick = false;
-});
-buttonDiv.appendChild(eraseButton);
-
-// Fills any space not colored by currentColor
 fillButton.className = 'buttons';
 fillButton.id = 'fillButton';
 fillButton.innerHTML = 'Fill';
+
+var canvasCellArr = document.getElementsByClassName('canvasCell');
+
 fillButton.addEventListener('click', function() {
-  var canvasCellArr = document.getElementsByClassName('canvasCell');
   for (var i = 0; i < canvasCellArr.length; i++) {
-    if (canvasCellArr[i].style.backgroundColor !== previousColor) {
-      console.log(currentColor);
+    //console.log(canvasCellArr[i].style.backgroundColor);
+    if (canvasCellArr[i].style.backgroundColor === "rgb(255, 255, 255)") {
       canvasCellArr[i].style.backgroundColor = currentColor;
-    };
-  };
+      console.log(currentColor);
+    }
+  }; 
   // console.log(currentColor);
 });
 buttonDiv.appendChild(fillButton);
 
-// Clears entire canvas and deselects current color
+eraseButton.className = 'buttons';
+eraseButton.id = 'eraseButton';
+eraseButton.innerHTML = 'Erase';
+eraseButton.addEventListener('click', function() {
+  currentColor = 'ffffff';
+  mouseClick = false;
+});
+buttonDiv.appendChild(eraseButton);
+
 clearButton.className = 'buttons';
 clearButton.id = 'clearButton';
 clearButton.innerHTML = 'Clear';
 clearButton.addEventListener('click', function() {
   for (var i = 0; i < document.getElementsByClassName('canvasCell').length; i++) {
-    document.getElementsByClassName('canvasCell')[i].style.backgroundColor = 'rgb(255, 255, 255)';
+    document.getElementsByClassName('canvasCell')[i].style.backgroundColor = 'ffffff'
   }
   resetPalette();
-  previousColor = 'rgb(255, 255, 255)'
-  currentColor = 'rgb(255, 255, 255)';
+  currentColor = 'ffffff';
   mouseClick = false;
 });
 buttonDiv.appendChild(clearButton);
 
-saveButton.id = "saveButton";
-saveButton.className = "buttons";
-saveButton.innerHTML = "Save";
-saveButton.addEventListener('click', savePic);
-buttonDiv.appendChild(saveButton);
-
-loadButton.id = "loadButton";
-loadButton.className = "buttons";
-loadButton.innerHTML = "Load";
-loadButton.addEventListener("click", loadPic);
-buttonDiv.appendChild(loadButton);
-
-// Functions to save and load pictures
-// Can only save one picture at a time
-// Saving another picture will rewrite current save
-function savePic(){
-  colorArray = [];
-  var currentPic = document.getElementsByClassName("canvasCell");
-  for (var i = 0; i < currentPic.length; i++) {
-    colorArray.push(currentPic[i].style.backgroundColor);
-  }
-  mouseClick = false;
-}
-
-function loadPic(){
-  var currentPic = document.getElementsByClassName("canvasCell");
-  for(var i = 0; i < currentPic.length; i++){
-    currentPic[i].style.backgroundColor = colorArray[i];
-  }
-  //colorArray = [];
-  mouseClick = false;
-}
-
-// Used to reset size of the palette cells when selecting a new color
 function resetPalette() {
   for (var i = 0; i < document.getElementsByClassName('paletteCell').length; i++) {
     document.getElementsByClassName('paletteCell')[i].style.width = '40px';
@@ -180,4 +136,40 @@ function resetPalette() {
     document.getElementsByClassName('paletteCell')[i].style.border = '0px';
   }
 }
+
+//CREATING GRID
+/*function ppCanvas(pixelNumber){
+  for(var i = 0; i < pixelNumber; i++){
+    var canvasCell = document.createElement("div");
+    canvasCell.className = "canvasCell";
+    painterCanvas.appendChild(canvasCell);
+    canvasCell.addEventListener("click", function() {
+      event.target.style.backgroundColor = currentColor;
+    })
+  }
+}
+*/
+//ppCanvas(99);
+
+//CREATING COLOR PALLETE
+/* function ppColor(pixelNumber){
+  for(var i = 0; i < pixelNumber; i++){
+    var paletteCell = document.createElement("div");
+    paletteCell.className = "paletteCell";
+    painterPalette.appendChild(paletteCell);
+    paletteCell.style.backgroundColor = colorArr[i];
+    paletteCell.addEventListener("click", function() {
+      currentColor = event.target.style.backgroundColor;
+      console.log(currentColor);
+    });
+  }
+}
+
+ppColor(16);
+*/
+
+
+
+
+
 
